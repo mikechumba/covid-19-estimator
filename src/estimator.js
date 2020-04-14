21e1749d16de64f60786d7540372e6bfc19ca505
Michael Kipchumba <michaelchumba09@gmail.com>
2020-04-14 23:31:18 +0300

    style: format code

--- src/estimator.js
+++ src/estimator.js
@@ -1,70 +1,84 @@
-
 class Impact {
-
   constructor(data) {
-      this.data = data;
+    this.data = data;
   }
 
   get impact() {
-      return this.calculateImpact(10);
+    return this.calculateImpact(10);
   }
 
   get severeImpact() {
-      return this.calculateImpact(50);
+    return this.calculateImpact(50);
   }
 
   calculateImpact(x) {
-      const { reportedCases, periodType, timeToElapse, totalHospitalBeds } = this.data;
-      const days = this.getDays(timeToElapse, periodType);
-      const currentlyInfected = reportedCases * x;
-      const setsOfThree = Math.floor(days/3);
-      const infectionsByRequestedTime = currentlyInfected * (2 ** setsOfThree);
-      const severeCasesByRequestedTime = infectionsByRequestedTime * 0.15;
-      const hospitalBedsByRequestedTime = Math.ceil(this.availableHospitalBeds() - severeCasesByRequestedTime);
-      return {
-          currentlyInfected: currentlyInfected,
-          infectionsByRequestedTime: infectionsByRequestedTime,
-          severeCasesByRequestedTime: severeCasesByRequestedTime,
-          hospitalBedsByRequestedTime: hospitalBedsByRequestedTime,
-          casesForICUByRequestedTime: this.casesForICUByRequestedTime(infectionsByRequestedTime),
-          casesForVentilatorsByRequestedTime: this.casesForVentilatorsByRequestedTime(infectionsByRequestedTime),
-          dollarsInFlight: this.dollarsInFlight(infectionsByRequestedTime, days)
-      }
+    const {
+      reportedCases,
+      periodType,
+      timeToElapse,
+      totalHospitalBeds
+    } = this.data;
+    const days = this.getDays(timeToElapse, periodType);
+    const currentlyInfected = reportedCases * x;
+    const setsOfThree = Math.floor(days / 3);
+    const infectionsByRequestedTime = currentlyInfected * 2 ** setsOfThree;
+    const severeCasesByRequestedTime = infectionsByRequestedTime * 0.15;
+    const hospitalBedsByRequestedTime = Math.ceil(
+      this.availableHospitalBeds() - severeCasesByRequestedTime
+    );
+    return {
+      currentlyInfected: currentlyInfected,
+      infectionsByRequestedTime: infectionsByRequestedTime,
+      severeCasesByRequestedTime: severeCasesByRequestedTime,
+      hospitalBedsByRequestedTime: hospitalBedsByRequestedTime,
+      casesForICUByRequestedTime: this.casesForICUByRequestedTime(
+        infectionsByRequestedTime
+      ),
+      casesForVentilatorsByRequestedTime: this.casesForVentilatorsByRequestedTime(
+        infectionsByRequestedTime
+      ),
+      dollarsInFlight: this.dollarsInFlight(infectionsByRequestedTime, days)
+    };
   }
 
   availableHospitalBeds() {
-      const { totalHospitalBeds } = this.data;
-      return totalHospitalBeds * 0.35;
+    const { totalHospitalBeds } = this.data;
+    return totalHospitalBeds * 0.35;
   }
 
   casesForICUByRequestedTime(infectionsByRequestedTime) {
-      return infectionsByRequestedTime * 0.05;
+    return infectionsByRequestedTime * 0.05;
   }
 
   casesForVentilatorsByRequestedTime(infectionsByRequestedTime) {
-      return Math.ceil(infectionsByRequestedTime * 0.02);
+    return Math.ceil(infectionsByRequestedTime * 0.02);
   }
 
   dollarsInFlight(infectionsByRequestedTime, timeToElapse) {
-      const { avgDailyIncomeInUSD, avgDailyIncomePopulation } = this.data.region;
-      return infectionsByRequestedTime * avgDailyIncomeInUSD * avgDailyIncomePopulation * timeToElapse
+    const { avgDailyIncomeInUSD, avgDailyIncomePopulation } = this.data.region;
+    return (
+      infectionsByRequestedTime *
+      avgDailyIncomeInUSD *
+      avgDailyIncomePopulation *
+      timeToElapse
+    );
   }
 
   getDays(time, periodType) {
-      if (periodType === 'days') {
-          return time;
-      } else if (periodType === 'weeks') {
-          return time * 7;
-      } else if (periodType === 'months') {
-          return time * 30;
-      }
+    if (periodType === 'days') {
+      return time;
+    } else if (periodType === 'weeks') {
+      return time * 7;
+    } else if (periodType === 'months') {
+      return time * 30;
+    }
   }
 }
 
 const covid19ImpactEstimator = (data) => {
-    const impact = new Impact(data);
+  const impact = new Impact(data);
 
-    return impact;
+  return impact;
 };
 
 export default covid19ImpactEstimator;
--- src/output.js
+++ src/output.js
@@ -1,62 +1,58 @@
 import covid19ImpactEstimator from './estimator.js';
 
 const regionData = {
-    region: {
-        name: 'Africa',
-        avgAge: 19.7,
-        avgDailyIncomeInUSD: 4,
-        avgDailyIncomePopulation: 0.71
-    }
+  region: {
+    name: 'Africa',
+    avgAge: 19.7,
+    avgDailyIncomeInUSD: 4,
+    avgDailyIncomePopulation: 0.71
+  }
 };
 
 const submitBtn = document.querySelector('#submitBtn');
 
 function serialize(nodes) {
-
-    return {
-        periodType: Number(nodes[4].dataset.periodType),
-        timeToElapse: Number(nodes[3].dataset.timeToElapse),
-        reportedCases: Number(nodes[2].dataset.reportedCases),
-        population: Number(nodes[0].dataset.population),
-        totalHospitalBeds: Number(nodes[1].dataset.totalHospitalBeds)
-    }
+  return {
+    periodType: Number(nodes[4].dataset.periodType),
+    timeToElapse: Number(nodes[3].dataset.timeToElapse),
+    reportedCases: Number(nodes[2].dataset.reportedCases),
+    population: Number(nodes[0].dataset.population),
+    totalHospitalBeds: Number(nodes[1].dataset.totalHospitalBeds)
+  };
 }
 
 submitBtn.addEventListener('click', (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    const inputNodes = document.querySelectorAll('.control');
+  const inputNodes = document.querySelectorAll('.control');
 
-    const formData = serialize(inputNodes);
+  const formData = serialize(inputNodes);
 
-    const validEntries = Object.entries(formData).filter(entry => {
+  const validEntries = Object.entries(formData).filter((entry) => {
+    return entry[1];
+  });
 
-        return entry[1];
-    })
-    
-    const data = { ...regionData, ...formData }
+  const data = { ...regionData, ...formData };
 
-    if (validEntries.length === 5) {
-        const output = covid19ImpactEstimator(data);
-        textToClipboard(JSON.stringify(output));
-    }
+  if (validEntries.length === 5) {
+    const output = covid19ImpactEstimator(data);
+    textToClipboard(JSON.stringify(output));
+  }
 });
 
 function textToClipboard(text) {
-    var dummy = document.createElement("textarea");
-    document.body.appendChild(dummy);
-    dummy.value = text;
-    dummy.select();
-    document.execCommand("copy");
-    document.body.removeChild(dummy);
+  var dummy = document.createElement('textarea');
+  document.body.appendChild(dummy);
+  dummy.value = text;
+  dummy.select();
+  document.execCommand('copy');
+  document.body.removeChild(dummy);
 
-    alert('Estimates copied to clipboard.')
+  alert('Estimates copied to clipboard.');
 }
 
 document.addEventListener('change', (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    e.target.dataset[e.target.name] = e.target.value
-})
-
-
+  e.target.dataset[e.target.name] = e.target.value;
+});
